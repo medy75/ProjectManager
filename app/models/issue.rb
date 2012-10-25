796d89d40100d6b917d6100b3827940d612faeda
medy75 <jarin.md75@gmail.com>
2012-10-25 16:34:56 +0200

    Added issue validation

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,5 @@
 class Issue < ActiveRecord::Base
   attr_accessible :description, :estimate, :label, :status, :title
+  validates :title, :presence => true
+  validates :estimate, :numericality => {:greater_than_or_equal_to => 0.1}
 end

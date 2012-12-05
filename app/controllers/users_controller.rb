69279f74839d79317530a1a89fd0f1acbcabf40f
medy75 <jarin.md75@gmail.com>
2012-12-06 00:20:03 +0100

    User controller modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    #@users = User.all
     @user = User.find(current_user.id)
   
     respond_to do |format|

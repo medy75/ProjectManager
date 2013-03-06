cc3b4af6a918615ad2b1d9bdf7198aa816ecc6bd
medy75 <jarin.md75@gmail.com>
2013-03-06 22:17:53 +0100

    Story points migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130227220547) do
+ActiveRecord::Schema.define(:version => 20130306211216) do
 
   create_table "issues", :force => true do |t|
     t.string   "title"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(:version => 20130227220547) do
     t.text     "description"
     t.string   "status"
     t.decimal  "estimate"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.integer  "project_id"
+    t.integer  "points_estimate"
   end
 
   create_table "projects", :force => true do |t|

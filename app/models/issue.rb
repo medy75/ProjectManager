cf84b39d60c4c5cbf1c165d9dff9b5e72e0967ab
medy75 <jarin.md75@gmail.com>
2013-03-06 23:38:11 +0100

    Refactored issue model and added new states

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ActiveRecord::Base
-  STATUS = ['new', 'in progress', 'testing', 'done']
+  STATUS = ['backlog', 'spritlog', 'new', 'in progress', 'testing', 'done']
   attr_accessible :description, :estimate, :label, :status, :title, :project_id, :points_estimate
   belongs_to :project
   validates :title, :presence => true

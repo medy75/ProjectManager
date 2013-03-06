7201c8982e68f9b604e4a4984d257b88df6b7ab1
medy75 <jarin.md75@gmail.com>
2013-03-06 22:21:33 +0100

    Issue model modified

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,7 @@
 class Issue < ActiveRecord::Base
-  attr_accessible :description, :estimate, :label, :status, :title, :project_id
+  attr_accessible :description, :estimate, :label, :status, :title, :project_id, :points_estimates
   belongs_to :project
   validates :title, :presence => true
   validates :estimate, :numericality => {:greater_than_or_equal_to => 0.1}
+  validates :points_estimate, :numericality => {:greater_than_or_equal_to => 0}
 end

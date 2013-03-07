9e3bd65bc2717cd86eeedc53535dd8a3029e25c5
medy75 <jarin.md75@gmail.com>
2013-03-07 23:04:57 +0100

    Added story points column for all issues

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,16 +15,32 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @issues = @project.issues
-    @undone_points = 0
+    @backlog_points = 0
+    @sprintlog_points = 0
+    @progress_points = 0
+    @test_points = 0
     @done_points = 0
 
-    @new_issues = @issues.select {|issue| issue.status=="new"}
+    @backlog_issues = @issues.select {|issue| issue.status=="backlog"}
+    @sprintlog_issues = @issues.select {|issue| issue.status=="sprintlog"}
     @progress_issues = @issues.select {|issue| issue.status=="in progress"}
     @test_issues = @issues.select {|issue| issue.status=="testing"}
     @done_issues = @issues.select {|issue| issue.status=="done"}
 
-    @new_issues.each do |new_issue|
-      @undone_points += new_issue.points_estimate if new_issue.points_estimate
+    @backlog_issues.each do |backlog_issue|
+      @backlog_points += backlog_issue.points_estimate if backlog_issue.points_estimate
+    end
+    @sprintlog_issues.each do |sprintlog_issue|
+      @sprintlog_points += sprintlog_issue.points_estimate if sprintlog_issue.points_estimate
+    end
+    @progress_issues.each do |progress_issue|
+      @progress_points += progress_issue.points_estimate if progress_issue.points_estimate
+    end
+    @test_issues.each do |test_issue|
+      @test_points += test_issue.points_estimate if test_issue.points_estimate
+    end
+    @done_issues.each do |done_issue|
+      @done_points += done_issue.points_estimate if done_issue.points_estimate
     end
     respond_to do |format|
       format.html # show.html.erb

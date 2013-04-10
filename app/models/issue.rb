6cdf4b66d4e9d6ac1e05d6d44170700545c1b054
medy75 <jarin.md75@gmail.com>
2013-04-10 22:25:46 +0200

    Changed issues layout

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ActiveRecord::Base
-  STATUS = ['backlog', 'spritlog', 'in progress', 'testing', 'done']
+  STATUS = ['backlog', 'sprintlog', 'in progress', 'testing', 'done']
   attr_accessible :description, :estimate, :label, :status, :title, :project_id, :points_estimate
   belongs_to :project
   validates :title, :presence => true

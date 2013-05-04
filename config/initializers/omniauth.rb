61775f151b4ee653aeeeb3cf77ef5b0ace1fd14a
medy75 <jarin.md75@gmail.com>
2013-05-04 19:40:34 +0200

    Try for Google auth #2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'AbKZLW3bBnQXGlQafTw2PQ', 'RFl5IImRBrXQkNShTeG6DBkI8tFwJW51I5MUgezxAGI'
-  provider :google, CONSUMER_KEY, CONSUMER_SECRET
 end

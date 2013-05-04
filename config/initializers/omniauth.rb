e2387039ce2cbddd02dc704fe29c4651797b31d1
medy75 <jarin.md75@gmail.com>
2013-05-04 19:52:41 +0200

    GOOGLE LOGIN JE TAAAM

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'AbKZLW3bBnQXGlQafTw2PQ', 'RFl5IImRBrXQkNShTeG6DBkI8tFwJW51I5MUgezxAGI'
+  provider :google, 'cvut-project-manager.herokuapp.com', '-M9raMm1-qylQaFE6uxn0dxi'
 end

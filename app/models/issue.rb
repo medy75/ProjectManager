9a509bc19619cbff15591a87a1615a7e67be0955
medy75 <jarin.md75@gmail.com>
2013-05-07 22:03:19 +0200

    Improve error message for issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,6 +3,6 @@ class Issue < ActiveRecord::Base
   attr_accessible :description, :estimate, :label, :status, :title, :project_id, :points_estimate
   belongs_to :project
   validates :title, :presence => true
-  validates :estimate, :numericality => {:greater_than_or_equal_to => 0.1}
+  validates :estimate, :numericality => {:greater_than_or_equal_to => 0}
   validates :points_estimate, :numericality => {:greater_than_or_equal_to => 0}
 end

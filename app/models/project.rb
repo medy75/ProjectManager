438f055c6beab93ad190013407b661dd92e73fa1
medy75 <jarin.md75@gmail.com>
2013-05-07 22:34:34 +0200

    Improve error message for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,5 @@ class Project < ActiveRecord::Base
   has_many :issues, :dependent => :destroy
   has_and_belongs_to_many :users, :uniq => true
   accepts_nested_attributes_for :issues, :allow_destroy => :true
+  validates :title, :presence => true
 end
